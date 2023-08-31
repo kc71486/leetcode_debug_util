d5c04b6b357359e0586bcc4983153d07ec703199
kc71486 <linda20030102@gmail.com>
2023-08-31 20:21:37 +0800

    cleanup

--- Solution.java
+++ Solution.java
@@ -1,16 +1,5 @@
-import java.util.Arrays;
+
 
 class Solution {
-    public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a, b) -> (a[1] - b[1]));
-        int count = 0;
-        int high = 0x80000000;
-        for(int i = 0; i < pairs.length; i ++) {
-            if(pairs[i][0] > high) {
-                count ++;
-                high = pairs[i][1];
-            }
-        }
-        return count;
-    }
+    
 }
--- Solution2.java
+++ Solution2.java
@@ -1,36 +1,5 @@
-import java.util.List;
-import java.util.ArrayList;
 
-import type.TreeNode;
 
 class Solution2 {
-    public List<TreeNode> generateTrees(int n) {
-        @SuppressWarnings("unchecked")
-        List<TreeNode>[] dp = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i] = new ArrayList<TreeNode>();
-        }
-        dp[0].add(null);
-        for (int curn = 1; curn <= n; curn++) {
-            for (int i = 1; i <= curn; i++) {
-                int j = curn - i;
-                for (TreeNode left : dp[i - 1]) {
-                    for (TreeNode right : dp[j]) {
-                        TreeNode root = new TreeNode(i, copyOffset(left, 0), copyOffset(right, i));
-                        dp[curn].add(root);
-                    }
-                }
-            }
-        }
-        return dp[n];
-    }
-    private TreeNode copyOffset(TreeNode node, int offset) {
-        if (node == null) {
-            return null;
-        }
-        TreeNode newnode = new TreeNode(node.val + offset);
-        newnode.left = copyOffset(node.left, offset);
-        newnode.right = copyOffset(node.right, offset);
-        return newnode;
-    }
+    
 }
